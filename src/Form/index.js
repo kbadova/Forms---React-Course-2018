f19a5ed93baffa44a70b777bdf7cc3c0e11e556f
kbadova <krasi@hacksoft.io>
2018-11-06 19:39:19 +0200

    Add more fields

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 class Form extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      name: ''
+      form: {
+        fields: {
+          name: {
+            value: ''
+          },
+          meal: {
+            value: ''
+          },
+          roomType: {
+            value: {
+              id: '',
+              name: ''
+            }
+          }
+        },
+        errors: []
+      },
+      userMessage: ''
     };
   }
+
+  post = ({url, payload}) => axios.post(url, payload);
 
   handleNameChange = event => {
     this.setState({name: event.target.value});
@@ -15,15 +35,77 @@ class Form extends React.Component {
 
   onEmailChange = event => {
     const emailValue = event.target.value;
-    console.log(emailValue);
+
+    const url = `http://localhost:8000/booking/email-exist/`;
+    const payload = {
+      email: emailValue
+    };
+
+    this.post({url, payload})
+      .then(response => {
+        const message = response.data.message;
+
+        this.setState({userMessage: message});
+      })
+      .catch(errors => {
+        const error = errors[0];
+
+        this.setState({
+          form: {
+            ...this.state.form,
+            errors: error
+          }
+        });
+      });
+  };
+
+  onPhoneChange = event =>
+    this.setState({
+      form: {
+        ...this.state.form,
+        fields: {
+          ...this.state.form.fields,
+          phone: {
+            value: event.target.value
+          }
+        }
+      }
+    });
+
+  onMealChange = event => {
+    const mealId = event.target.value;
+
+    const {meals, form} = this.state;
+
+    const mealOption = meals.filter(meal => meal.id === mealId);
+
+    this.setState({
+      form: {
+        ...form,
+        fields: {
+          ...form.fields,
+          meal: {
+            ...form.fields.meal,
+            value: mealOption[0]
+          }
+        }
+      }
+    });
   };
 
   handleSubmit = () => {};
 
   render() {
-    const {name, email} = this.state;
+    const {
+      name,
+      email,
+      userMessage,
+      form: {
+        fields: {meal, roomType}
+      },
+      meals
+    } = this.state;
 
-    console.log(name);
     return (
       <form>
         <label>Name</label>
@@ -40,6 +122,26 @@ class Form extends React.Component {
           onChange={this.onEmailChange}
           placeholder="Type your email"
         />
+
+        {userMessage && <div>{userMessage}</div>}
+
+        <label>Phone</label>
+        <input
+          type="text"
+          onChange={this.onPhoneChange}
+          placeholder="Type your phone"
+        />
+
+        <label>Meal</label>
+        <select onChange={this.onMealChange}>
+          {meals &&
+            meals.map(meal => <option value={meal.id}>{meal.name}</option>)}
+        </select>
+
+        <label>Room Type</label>
+        <select onChange={this.onRoomTypeChange}>
+          <option value={roomType.id}>{roomType.name}</option>
+        </select>
 
         <button onClick={this.handleSubmit}>Request Booking</button>
       </form>
