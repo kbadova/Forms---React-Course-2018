38288f77c6df7da2de5d0064c86aa6789c19cee1
kbadova <krasi@hacksoft.io>
2018-11-07 20:01:44 +0200

    Add more fields

--- src/Form/index.js
+++ src/Form/index.js
@@ -16,13 +16,32 @@ class Form extends React.Component {
           },
           roomType: {
             value: ''
+          },
+          notes: {
+            value: ''
+          },
+          guests: {
+            value: ''
+          },
+          phone: {
+            value: ''
+          },
+          email: {
+            value: ''
+          },
+          start: {
+            value: ''
+          },
+          end: {
+            value: ''
           }
         },
         errors: []
       },
       userMessage: '',
       meals: [],
-      roomTypes: []
+      roomTypes: [],
+      successMessage: ''
     };
   }
 
@@ -73,9 +92,9 @@ class Form extends React.Component {
       form: {
         ...this.state.form,
         fields: {
-          ...this.state.fields,
+          ...this.state.form.fields,
           name: {
-            ...this.state.fields.name,
+            ...this.state.form.fields.name,
             value: event.target.value
           }
         }
@@ -83,30 +102,48 @@ class Form extends React.Component {
     });
   };
 
+  // onEmailChange = event => {
+  //   const emailValue = event.target.value;
+
+  //   const url = `http://localhost:8000/booking/email-exist/`;
+  //   const payload = {
+  //     email: emailValue
+  //   };
+
+  //   this.post({url, payload})
+  //     .then(response => {
+  //       const message = response.data.message;
+
+  //       this.setState({userMessage: message});
+  //     })
+  //     .catch(errors => {
+  //       const error = errors[0];
+
+  //       this.setState({
+  //         form: {
+  //           ...this.state.form,
+  //           errors: error
+  //         }
+  //       });
+  //     });
+  // };
+
   onEmailChange = event => {
     const emailValue = event.target.value;
+    const {form} = this.state;
 
-    const url = `http://localhost:8000/booking/email-exist/`;
-    const payload = {
-      email: emailValue
-    };
-
-    this.post({url, payload})
-      .then(response => {
-        const message = response.data.message;
-
-        this.setState({userMessage: message});
-      })
-      .catch(errors => {
-        const error = errors[0];
-
-        this.setState({
-          form: {
-            ...this.state.form,
-            errors: error
+    this.setState({
+      form: {
+        ...form,
+        fields: {
+          ...form.fields,
+          email: {
+            ...form.fields.email,
+            value: emailValue
           }
-        });
-      });
+        }
+      }
+    });
   };
 
   onPhoneChange = event =>
@@ -125,7 +162,6 @@ class Form extends React.Component {
 
   onMealChange = event => {
     const mealId = event.target.value;
-
     const {form} = this.state;
 
     this.setState({
@@ -161,67 +197,212 @@ class Form extends React.Component {
     });
   };
 
-  handleSubmit = () => {};
+  handleNotesChange = event => {
+    const notesValue = event.target.value;
+
+    this.setState({
+      form: {
+        ...this.state.form,
+        fields: {
+          ...this.state.form.fields,
+          notes: {
+            ...this.state.form.fields.notes,
+            value: notesValue
+          }
+        }
+      }
+    });
+  };
+
+  onGuestsChange = event => {
+    const guestsValue = event.target.value;
+    const {form} = this.state;
+
+    this.setState({
+      form: {
+        ...form,
+        fields: {
+          ...form.fields,
+          guests: {
+            ...form.fields.guests,
+            value: guestsValue
+          }
+        }
+      }
+    });
+  };
+
+  handleErrors = errors => {
+    // field errors
+    const reponseData = errors.response.data;
+
+    console.log(reponseData);
+  };
+
+  handleSubmit = () => {
+    const {
+      form: {
+        fields: {name, meal, email, phone, roomType, notes, guests, start, end}
+      }
+    } = this.state;
+
+    const data = {
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      meal: meal.value,
+      roomType: roomType.value,
+      notes: notes.value,
+      numberOfPeople: guests.value,
+      start: start.value,
+      end: end.value
+    };
+
+    const url = 'http://localhost:8000/booking/booking-request/';
+    axios
+      .post(url, data)
+      .then(response => {
+        this.setState({successMessage: 'Booking request sent!'});
+      })
+      .catch(errors => {
+        this.handleErrors(errors);
+      });
+  };
+
+  handleStartChange = event => {
+    const start = event.target.value;
+    const {form} = this.state;
+
+    this.setState({
+      form: {
+        ...form,
+        fields: {
+          ...form.fields,
+          start: {
+            ...form.fields.start,
+            value: start
+          }
+        }
+      }
+    });
+  };
+
+  handleEndChange = event => {
+    const end = event.target.value;
+    const {form} = this.state;
+
+    this.setState({
+      form: {
+        ...form,
+        fields: {
+          ...form.fields,
+          end: {
+            ...form.fields.end,
+            value: end
+          }
+        }
+      }
+    });
+  };
 
   render() {
     const {
       userMessage,
       form: {
-        fields: {name}
+        fields: {name, notes, guests, phone, email, start, end}
       },
       meals,
-      roomTypes
+      roomTypes,
+      successMessage
     } = this.state;
 
     return (
-      <form>
-        <label>Name</label>
-        <input
-          type="text"
-          value={name.value}
-          onChange={this.handleNameChange}
-          placeholder="Type your name"
-        />
+      <React.Fragment>
+        <form>
+          <label>Name</label>
+          <input
+            type="text"
+            value={name.value}
+            onChange={this.handleNameChange}
+            placeholder="Type your name"
+          />
 
-        <label>Email</label>
-        <input
-          type="email"
-          onChange={this.onEmailChange}
-          placeholder="Type your email"
-        />
+          <label>Email</label>
+          <input
+            type="email"
+            value={email.value}
+            onChange={this.onEmailChange}
+            placeholder="Type your email"
+          />
 
-        {userMessage && <div>{userMessage}</div>}
+          <label>Start</label>
+          <input
+            type="date"
+            value={start.value}
+            onChange={this.handleStartChange}
+          />
 
-        <label>Phone</label>
-        <input
-          type="text"
-          onChange={this.onPhoneChange}
-          placeholder="Type your phone"
-        />
+          <label>End</label>
+          <input
+            type="date"
+            value={end.value}
+            onChange={this.handleEndChange}
+          />
 
-        <label>Meal</label>
-        <select onChange={this.onMealChange}>
-          {meals &&
-            meals.map(meal => (
-              <option key={meal.id} value={meal.id}>
-                {meal.name}
-              </option>
-            ))}
-        </select>
+          {userMessage && <div>{userMessage}</div>}
+          <label>Phone</label>
+          <input
+            type="text"
+            value={phone.value}
+            onChange={this.onPhoneChange}
+            placeholder="Type your phone"
+          />
 
-        <label>Room Type</label>
-        <select onChange={this.onRoomTypeChange}>
-          {roomTypes.map(roomType => {
-            return (
-              <option key={roomType.id} value={roomType.id}>
-                {roomType.name}
-              </option>
-            );
-          })}
-        </select>
+          <label>Room Type</label>
+          <select onChange={this.onRoomTypeChange}>
+            {roomTypes.map(roomType => {
+              return (
+                <option key={roomType.id} value={roomType.id}>
+                  {roomType.name}
+                </option>
+              );
+            })}
+          </select>
 
-        <button onClick={this.handleSubmit}>Request Booking</button>
-      </form>
+          <label>Meal</label>
+          {meals.map(meal => (
+            <React.Fragment key={meal.id}>
+              <label>{meal.name}</label>
+              <input
+                key={meal.id}
+                type="radio"
+                value={meal.id}
+                name="radio"
+                onChange={this.onMealChange}
+              />
+            </React.Fragment>
+          ))}
+
+          <label>Guests</label>
+          <input
+            type="number"
+            value={guests.value}
+            onChange={this.onGuestsChange}
+          />
+
+          <label>Notes</label>
+          <textarea
+            placeholder="White you notes here..."
+            onChange={this.handleNotesChange}
+            value={notes.value}
+          />
+          <button onClick={this.handleSubmit} type="button">
+            Request Booking
+          </button>
+        </form>
+
+        {successMessage && <div>{successMessage}</div>}
+      </React.Fragment>
     );
   }
 }
